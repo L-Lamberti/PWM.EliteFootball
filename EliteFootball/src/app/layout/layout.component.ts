285df4427d76f0a6b083dd535d758ae68875418a
LightLuke01 <luca.lamberti@community.unipa.it>
2025-06-04 01:48:32 +0200

    logo

--- EliteFootball/src/app/layout/layout.component.ts
+++ EliteFootball/src/app/layout/layout.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { Router } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
